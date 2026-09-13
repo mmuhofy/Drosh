1e9fd37bc003c2f99421885db541e9ba6f267d04
Muhofy <muhofy@iris.local>
2026-09-13 00:45:50 +0000

    fix: remove leftover IntOffset import

--- app/src/main/java/com/iris/irisshell/MainActivity.kt
+++ app/src/main/java/com/iris/irisshell/MainActivity.kt
@@ -23,7 +23,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.IntOffset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
